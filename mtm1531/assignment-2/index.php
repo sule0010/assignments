c903915ee6dc1b3b8896bc742260c374ea6a8760
sule0010 <bashir.b.suleiman@gmail.com>
2012-05-14 15:41:53 -0400

    Created a form.

--- mtm1531/assignment-2/index.php
+++ mtm1531/assignment-2/index.php
@@ -6,5 +6,19 @@
 	</head>
 	
 	<body>
+	
+	<form method="post" action="index.php">
+			<label id="number-one">Number 1</label>
+			<input type="number" name="number-one">
+			
+			<label id="number-two">Number 2</label>
+			<input type="number" name="number-two">
+			
+			<label id="number-two">function</label>
+			<input type="function" name="number-two">
+				
+			<button type="submit">Calculate</button>
+		</form>
+	
 	</body>
 </html>
