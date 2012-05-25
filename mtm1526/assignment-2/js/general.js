0eeeb15289d8a5744fc632e99fec6afdb2fafe20
Bashir Bello Suleiman <bashir.b.suleiman@gmail.com>
2012-05-25 00:08:28 -0400

    added a few lines of code to the general.js

--- mtm1526/assignment-2/js/general.js
+++ mtm1526/assignment-2/js/general.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+var addBtn = document.getElementById('add-btn');
+
+var userInput = document.getElementById('userInput');
+
+var bind = function(elem, ev, func) {
+	if(window.addEventListener){
+		elem.addEventListener(ev, func, false);
+	}
+	else{
+		elem.attachEvent('on' + ev, func);
+	}
+};
+
+bind(addBtn, 'click', function() {
+	list = document.createElement('li');
+	document.getElementById('container').appendChild(list);
+	list.innerHTML = userInput.value;
+	//removeCurrent();
+	list.className = 'current';
+	//alert('I was clicked!');
+});
