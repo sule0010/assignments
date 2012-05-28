39c43d5be9b0d8a2c2cf574ee31db81413abf7d4
Bashir Bello Suleiman <bashir.b.suleiman@gmail.com>
2012-05-27 22:33:20 -0400

    Created a switch statement to control the properties in the general.js file.

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,6 +1,20 @@
-// JavaScript Document
-$('form').on('submit', function (ev) {
-	ev.preventDefault();
-	
-
-});
+// JavaScript Document	
+	$('form').on('submit', function (ev) {
+		ev.preventDefault();
+		
+		var colorSelect = $('#colorSelect').val();
+		
+		switch($('#properties').val()) {
+			case 'border-color' :
+			$('#circle').css('border-color', colorSelect);
+			break;
+			
+			case 'background-color' :
+			$('#circle').css('background-color', colorSelect);
+			break;
+			
+			case 'color' :
+			$('#circle').css('color', colorSelect);
+			break;
+		};
+	});
