15580a4f08f114c4478854514a8c77476b953d74
Bashir Bello Suleiman <bashir.b.suleiman@gmail.com>
2012-05-30 01:19:35 -0400

    changed the folder name from js to include.

---
+++ mtm1531/assignment-3/includes/form-process.php
@@ -0,0 +1,38 @@
+<?php
+
+$errors = array();
+
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$uName = filter_input(INPUT_POST, 'uName', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+$lang = filter_input(INPUT_POST, 'lang', FILTER_SANITIZE_STRING);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+	if(empty($name)) {
+		$errors['name'] = true;
+	}
+	if(empty($uName)) {
+		$errors['uName'] = true;
+	}
+	
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = true;
+	}
+	
+	if(mb_strlen($message) < 5 || mb_strlen($message) > 100) {
+		$errors['message'] = true;
+	}
+	if(!isset($_POST['terms'])) {
+		$errors['terms'] = true;
+	}
+	if ($lang != 'eng' || $lang != 'frn' || $lang != 'esp') {
+		$errors['lang'] = true;
+	}
+	if(empty($errors)) {
+		$headers = 'From: ' . $name . ' <' . $email . ' >';
+		mail('kurodeshiro@gmail.com', $uName, $message, $headers);  
+	}
+}
+?>
