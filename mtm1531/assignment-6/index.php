b2c200dc9355c325dcc5897760f2ec566f8bb9ab
sule0010 <bashir.b.suleiman@gmail.com>
2012-06-11 17:58:21 -0400

    added the delete, index, single, general and db files.

---
+++ mtm1531/assignment-6/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'includes/db.php';
+
+$sql = $db->query('
+	SELECT id, title, release_date, director, genre, rating
+	FROM movies
+	ORDER BY title ASC
+');
+
+$results = $sql->fetchAll();
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Movies</title>
+		<link href="css/general.css" rel="stylesheet">
+	</head>
+	<body>
+	<div class="wrapper">
+		<h1>Movies</h1>
+		<?php foreach ($results as $movies) : ?>
+			<h2><a href="single.php?id=<?php echo $movies['id']; ?>"><?php echo $movies['title']; ?></a></h2>
+			<dl>
+				<dt>Release Date</dt>
+				<dd><?php echo $movies['release_date']; ?></dd>
+				<dt>Director</dt>
+				<dd><?php echo $movies['director']; ?></dd>
+				<dt>Genre</dt>
+				<dd><?php echo $movies['genre']; ?></dd>
+				<dt>Rating</dt>
+				<dd><?php echo $movies['rating']; ?></dd>
+			</dl>
+		<?php endforeach; ?>
+	</div>
+	</body>
+</html>
