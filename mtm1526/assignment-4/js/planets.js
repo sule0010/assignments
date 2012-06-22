aa336957870780c73694048a1873cfd330d94274
sule0010 <bashir.b.suleiman@gmail.com>
2012-06-22 13:06:25 -0400

    adjusted a few code of lines to make the current appear only on one tab and styled the background to match it.

--- mtm1526/assignment-4/js/planets.js
+++ mtm1526/assignment-4/js/planets.js
@@ -1,30 +1,28 @@
 // JavaScript Document
 $(document).ready(function(){
-
-	var marker;
 	
 	$.get('earth.html', function(data){
 		$('div').prepend(data);
 	});
-	$('#earth').on('click', function(e){
-		marker = e.target;
-		marker.className = 'current';
+	$('#earth').on('click', function(){
 		$('#fillme').load('earth.html');
+		$('nav li').removeClass('current');
+		$('#earth').addClass('current');
 	});
-	$('#mars').on('click', function(e){
-		marker = e.target;
-		marker.className = 'current';
+	$('#mars').on('click', function(){
 		$('#fillme').load('mars.html');
+		$('nav li').removeClass('current');
+		$('#mars').addClass('current');
 	});
-	$('#jupiter').on('click', function(e){
-		marker = e.target;
-		marker.className = 'current';
+	$('#jupiter').on('click', function(){
 		$('#fillme').load('jupiter.html');
+		$('nav li').removeClass('current');
+		$('#jupiter').addClass('current');
 	});
-	$('#venus').on('click', function(e){
-		marker = e.target;
-		marker.className = 'current';
+	$('#venus').on('click', function(){
 		$('#fillme').load('venus.html');
+		$('nav li').removeClass('current');
+		$('#venus').addClass('current');
 	});
 	
 });
