8f6e802375a3af8e543b63e72f332b9ae6477630
Bashir Bello Suleiman <bashir.b.suleiman@gmail.com>
2012-06-22 01:31:41 -0400

    created a variable called marker to keep track of the current tab one is on.

--- mtm1526/assignment-4/js/planets.js
+++ mtm1526/assignment-4/js/planets.js
@@ -1,18 +1,29 @@
 // JavaScript Document
 $(document).ready(function(){
+
+	var marker;
+	
 	$.get('earth.html', function(data){
 		$('div').prepend(data);
 	});
-	$('#earth').on('click', function(){
+	$('#earth').on('click', function(e){
+		marker = e.target;
+		marker.className = 'current';
 		$('#fillme').load('earth.html');
 	});
-	$('#mars').on('click', function(){
+	$('#mars').on('click', function(e){
+		marker = e.target;
+		marker.className = 'current';
 		$('#fillme').load('mars.html');
 	});
-	$('#jupiter').on('click', function(){
+	$('#jupiter').on('click', function(e){
+		marker = e.target;
+		marker.className = 'current';
 		$('#fillme').load('jupiter.html');
 	});
-	$('#venus').on('click', function(){
+	$('#venus').on('click', function(e){
+		marker = e.target;
+		marker.className = 'current';
 		$('#fillme').load('venus.html');
 	});
 	
