1f0029dcd20dea270ca547fca7de473d655b67f8
sule0010 <bashir.b.suleiman@gmail.com>
2012-06-29 09:43:47 -0400

    added the index.php, general.css, earth.php, jupiter.php, mars.php, venus.php, planets.js

---
+++ mtm1526/assignment-6/index.php
@@ -0,0 +1,51 @@
+<?php
+			$planet = '';
+			 
+			if (isset($_GET['planet'])) {
+				$planet = strtolower($_GET['planet']);
+			}
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Planets</title>
+		<link href="css/general.css" rel="stylesheet">
+	</head>
+	
+	<body>
+	
+		<nav>
+			<ul>
+				<li<?php if ($planet != 'mars' && $planet != 'jupiter' && $planet != 'venus') {?> class="current"<?php } ?>><a href="?planet=earth">Earth</a></li>
+				<li<?php if ($planet == 'mars') {?> class="current"<?php } ?>><a href="?planet=mars">Mars</a></li>
+				<li<?php if ($planet == 'jupiter') {?> class="current"<?php } ?>><a href="?planet=jupiter">Jupiter</a></li>
+				<li<?php if ($planet == 'venus') {?> class="current"<?php } ?>><a href="?planet=venus">Venus</a></li>
+			</ul>
+		</nav>
+		
+		<article>
+		
+		<?php
+			switch ($planet) {
+				case 'mars' :
+					include 'includes/mars.php';
+				break;
+				case 'jupiter' :
+					include 'includes/jupiter.php';
+				break;
+				case 'venus' :
+					include 'includes/venus.php';
+				break;
+				case 'earth' :
+				default : 
+					include 'includes/earth.php';
+				break;
+			}
+		?>
+		</article>
+		<div id="fillme"></div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script src="js/planets.js"></script>
+	</body>
+</html>
